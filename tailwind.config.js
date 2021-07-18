ddb14f06eed2ba9b6b39f08cf54a230a739cbabc
Ertan Mutlu <ertansofia@gmail.com>
2021-07-18 22:50:27 +0300

    pre js commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './build/**/*.html',
+    './build/**/*.ts',
+    './build/**/*.jsx',
+    './build/**/*.js',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
